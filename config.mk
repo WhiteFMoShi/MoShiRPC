77d5752c844cdb4fcb6674957cafe6d62c604256
xfy's WSL <3511784747@qq.com>
2025-08-12 11:06:58 +0800

    fix(config): fix bug

--- config.mk
+++ config.mk
@@ -1,10 +1,17 @@
 ################### 全局控制 ###################
 PROJECT_ROOT = $(abspath $(dir $(lastword $(MAKEFILE_LIST))))
+
 BUILD_PATH = $(PROJECT_ROOT)/build
 BIN_PATH := $(BUILD_PATH)/bin
 LIBS_PATH := $(BUILD_PATH)/lib
-SDK_PATH := $(PROJECT_ROOT)/SDK
-INCLUDE_PATH := -Isrc -Iinclude -I$(SDK_PATH)
+THIRD_PARTY_PATH := $(PROJECT_ROOT)/third_party
+INCLUDE := include
+SRC := src
+
+INCLUDE_PATH := -I$(SRC) -I$(INCLUDE) -I$(THIRD_PARTY_PATH) \
+				-I$(shell find $(PROJECT_ROOT) -name log)/$(INCLUDE) \
+				-I$(shell find $(PROJECT_ROOT) -name config_loader)/$(INCLUDE)
+OJBS_PATH := $(BUILD_PATH)/obj
 
 CXX := g++
-CXXFLAGS := -Wall -O3 $(INCLUDE_PATH) -g
+CXXFLAGS := -Wall -g -std=c++17 $(INCLUDE_PATH)

c7d26033a15a7546a696072ec2171a9503c7ebb4
xfy's WSL <3511784747@qq.com>
2025-08-14 12:21:32 +0800

    fix(log): 优化log.hpp的注释，提高可读性

--- log/include/log.hpp
+++ log/include/log.hpp
@@ -22,20 +22,22 @@ public:
     static Log& getInstance();
 
     /**
-    * @brief Add a new Entry to Log's queue.
-    *
-    * @param level The entry Level, can checking in LogLevel.
-    * @param module which module add this entry.
-    * @param msg The message of this entry.
-    * @exception When calling addLog after using close(), it will throw out a std::runtimeerror.
-                 It may happen Sin multiple-threads enviorment.
-    */
+     * @brief 向日志队列中添加一条日志条目
+     *
+     * @param level 日志级别，可参考 LogLevel 枚举定义。
+     * @param module 发起该日志的模块名称。
+     * @param msg 日志的具体消息内容。
+     * @exception 如果在调用 close() 方法关闭日志后，再调用 addLog()，则会抛出 std::runtime_error 异常。
+     *            该情况可能发生在多线程环境下。
+     */
     void addLog(LogLevel level, std::string module, const std::string& msg);
 
     /**
-     * @brief 根据日志级别，单独输出日志于terminal中
+     * @brief 仅将日志信息打印到终端，而不写入到文件中
      * 
-     * @param msg 要输出的日志信息
+     * @param level 该条目的日志级别
+     * @param module 模块信息
+     * @param msg 日志信息
      */
     static void terminal_log(LogLevel level, const std::string& module, const std::string& msg);
 
@@ -56,9 +58,10 @@ private:
     ~Log();
 
     /**
-    * @brief Close the threadpool, cleaning all entry in queue.
-    * @warning This function shouldn't called derictly, because the open function haven't implement.
-    */
+     * @brief 关闭线程池，并清理队列中的所有日志条目。
+     * 
+     * @warning 此函数不应被直接调用，因为 open() 函数尚未实现。
+     */
     void close();
 };
 

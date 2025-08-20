196cef5bd8cb14410228e14792c69d5695cf4f93
xfy's WSL <3511784747@qq.com>
2025-08-21 00:48:16 +0800

    optimize(rpc/protocol): 优化注释信息，提高可读性

--- rpc/protocol/include/protocol.hpp
+++ rpc/protocol/include/protocol.hpp
@@ -1,61 +1,71 @@
 #pragma once
 
 #include <cstdint>
-#include <string>
+#include <memory>
 #include <vector>
 
 #include "cJSON/cJSON.h"
 
+namespace moshi {
 const uint32_t PROTOCOL_VERSION_1 = 0x6A5D4C7E;
+constexpr int HEADER_LENGTH = 2 * sizeof(uint32_t);
 
-namespace MoShi {
+constexpr uint32_t MAX_ALLOWED_LENGTH = 1024 * 1024; // 1MB
 
 class Protocol {
-    friend Protocol Deserialize(std::string network_string);
+    friend Protocol Deserialize(std::vector<char>, int& p);
+    friend void print_network_stream(const Protocol& ptl);
+    friend void print_protocol(const Protocol& ptl);
 public:
     /**
-    * @brief Construct a Protocol object from a cJSON item.
-    * 
-    * @param item Pointer to a cJSON object containing the data to be sent over the network.
-    *                The caller retains ownership of the cJSON tree; this constructor will
-    *                serialize the data into an internal buffer (data_).
-    * @note This constructor will deep-copy the JSON content. For performance-critical scenarios,
-    *       consider using Protocol(const std::string&) with pre-serialized JSON.
-    * @warning The input `item` must be a valid cJSON object (non-null and properly formatted).
-    */
+     * @brief 从一个 cJSON 项构造一个 Protocol 对象
+     * 
+     * @param item 指向一个cJSON对象的指针，该对象包含要通过网络发送的数据
+     *             调用者保留cJSON树的所有权；此构造函数将把数据序列化到内部缓冲区（data_）中
+     * @note 此构造函数不会对JSON内容进行深拷贝，而是直接使用std::shared_ptr接管item，因此无需再使用cJSON_Delete手动释放
+     * @warning 输入的item必须是一个有效的cJSON对象（非空且格式正确）
+     */
     Protocol(cJSON* item);
 
     /**
-    * @brief Construct a Protocol object by parsing a network text stream.
-    * @param msg The raw message received from the network, expected to be in the format:
-    *               - Header (magic number + length) + JSON payload (if text mode is enabled).
-    *               - Or a pure JSON string (if the protocol is configured for text-only mode).
-    */
-    Protocol(std::string msg);
-
-    /**
-     * @brief Uses RAII to automatically call cJSON_Delete() and release the memory of data_json_.
+     * @brief 解析网络字节流，将其中的数据转换为cJSON对象，并封装到Protocol结构体中
+     *
+     * @param str 从网络接收到的原始消息，格式为：头部（魔数+长度）+JSON文本
+     * @param p 一个指针，用于指示解析到的位置。
      */
-    ~Protocol();
+    Protocol(const std::vector<char>& str, int& p);
+
+    Protocol& operator=(Protocol&&) = default;
 
     /**
-     * @brief Serialize Protocol object to Network text string.
-     * @return std::vector<char> 
-     * @throw runtime_error When cJSON_Print() return NULL
+     * @brief 将Protocol类型的数据序列化为字节流，存储在vector<char>中，便于发向网络
+     *
+     * @return std::vector<char>
+     * @throw 抛出std::runtime_error当cJSON_Print()返回NULL时
      */
     std::vector<char> Serialize() const; 
-    std::string Print() const;
+
     /**
-     * @brief Replace item to data_json_, and delete old data_json_.
+     * @brief 重设data_json
+     *
      * @param item 
      */
     void reset_json(cJSON* item);
 private:
-    uint32_t magic_; // Protocol identifier
-    std::string data_string_; // Json to string
-    cJSON* data_json_; // resource data
+    uint32_t magic_; // Protocol version
+    std::shared_ptr<cJSON> data_json_; // json data
 };
 
-Protocol Deserialize(std::string network_string);
+void print_network_stream(const Protocol& ptl);
+void print_protocol(const Protocol& ptl);
 
-} // namespace MoShi
+/**
+ * @brief 将网络字节流反序列化为Protocol
+ * 
+ * @param network_string 接收的网络字节流
+ * @param p 用于标记str中，数据的处理位置
+ * @return Protocol 
+ */
+Protocol Deserialize(const std::vector<char>& network_string, int& p);
+
+} // namespace moshi
--- rpc/protocol/src/protocol.cpp
+++ rpc/protocol/src/protocol.cpp
@@ -1,9 +1,10 @@
 #include <cstdint>
+#include <cstdio>
 #include <cstdlib>
 #include <memory>
 #include <stdexcept>
 #include <vector>
-#include <string>
+#include <iostream>
 #include <cstring>
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -11,18 +12,39 @@
 #include "protocol.hpp"
 #include "cJSON/cJSON.h"
 
-using namespace MoShi;
-
-Protocol::Protocol(cJSON* item) {
-    magic_ = PROTOCOL_VERSION_1;
-    data_json_ = item;
+moshi::Protocol::Protocol(cJSON* item) : magic_(PROTOCOL_VERSION_1) {
+    data_json_ = std::shared_ptr<cJSON>(item, cJSON_Delete);
 }
 
-Protocol::~Protocol() {
-    cJSON_Delete(data_json_);
+/*
+    现在使用p来进行字符串划分，能在一定程度上解决粘包问题
+*/
+moshi::Protocol::Protocol(const std::vector<char>& str, int& p) {
+    uint32_t magic_net, length_net;
+    memcpy(&magic_net, str.data(), sizeof(magic_net));
+    memcpy(&length_net, str.data() + sizeof(magic_net), sizeof(length_net));
+
+    magic_ = ntohl(magic_net);
+    uint32_t data_length = htonl(length_net);
+
+    if(data_length > MAX_ALLOWED_LENGTH) {
+        throw std::runtime_error("Prortocol::Protocol(const std::vector<char>&): Invalid data length!!!)");
+    }
+
+    p += HEADER_LENGTH + data_length;
+
+    if(magic_ != PROTOCOL_VERSION_1) 
+        throw std::runtime_error("Protocol::Protocol(const std::vector<char>&): Invalid magic number!");
+
+    cJSON* data_parsed = cJSON_ParseWithLength(str.data() + HEADER_LENGTH, data_length);
+    if(data_parsed == nullptr) {
+        throw std::runtime_error("Protocol::Protocol(const std::vector<char>&): Failure parse data!");
+    }
+    data_json_ = std::shared_ptr<cJSON>(data_parsed,
+                                        cJSON_Delete); // 指定删除器
 }
 
-std::vector<char> Protocol::Serialize() const {
+std::vector<char> moshi::Protocol::Serialize() const {
     class FreeDeleter {
     public:
         void operator()(char* ptr) const {
@@ -32,11 +54,11 @@ std::vector<char> Protocol::Serialize() const {
 
     uint32_t magic_net = htonl(magic_);
     // data_net可以实现一个包装器进行优化，就不用手动free了
-    char* data_net = cJSON_Print(data_json_);
+    char* data_net = cJSON_Print(data_json_.get());
     // char[]和char*还是不同的，在delete的时候，前者释放整个字符串，后者只释放指针
     std::unique_ptr<char[], FreeDeleter> data_guard(data_net);
     if(data_net == nullptr) {
-        throw std::runtime_error("protocol::Serialize Json parse error!");
+        throw std::runtime_error("protocol::Serialize(): Failure parse to Json!");
     }
     uint32_t data_length = static_cast<uint32_t>(strlen(data_net));
     uint32_t data_length_net = htonl(data_length);
@@ -52,13 +74,38 @@ std::vector<char> Protocol::Serialize() const {
     return buffer;
 }
 
-void Protocol::reset_json(cJSON* item) {
-    cJSON_Delete(data_json_);
-    data_json_ = item;
+void moshi::Protocol::reset_json(cJSON* item) {
+    if(item == nullptr) {
+        throw std::invalid_argument("Protocol::reset_json(cJSON*): The argument is nullptr!");
+    }
+    data_json_.reset(item, cJSON_Delete); // 安全释放旧指针，接管新指针
 }
 
-Protocol Deserialize(std::string network_string) {
-    char* ptr = network_string.data();
-    uint len = network_string.size();
-    uint32_t magic = static_cast<uint32_t>(*ptr);   
+void moshi::print_network_stream(const Protocol& ptl) {
+    // 输出的应该是Json字符流加上前面8字节的二进制数
+    int i = 0;
+    auto str = ptl.Serialize();
+    for(auto& temp : str) {
+        if(i < 8) {
+            // 前8个字节可能会对应ASCII中的特殊字符，打印不出来，只能使用16进制
+            printf("%02x ", temp);
+            i++;
+        }
+        else
+            printf("%c", temp);
+    }
+    std::cout << std::endl;
+}
+
+void moshi::print_protocol(const Protocol& ptl) {
+    printf("%02x ", ptl.magic_);
+    char* str = cJSON_Print(ptl.data_json_.get());
+    printf("\n Json: %s", str);
+    free(str);
+}
+
+// 这里存在一个拷贝问题，不知道是否会影响，若是编译器优化了return不会将对象释放，就不会有问题
+// 后续应该换成完美转发来处理吧？
+moshi::Protocol moshi::Deserialize(const std::vector<char>& network_string, int& p) {
+    return Protocol(network_string, p);
 }

4a8db0dede0d86504607939a26d4bcd908544665
xfy's WSL <3511784747@qq.com>
2025-09-18 22:41:59 +0800

    build(third_party):更新代码逻辑，确保能正常完成编译操作

--- third_party/construction.sh
+++ third_party/construction.sh
@@ -1,26 +1,29 @@
 #!/usr/bin/env bash
 
+######################## cJSON ########################
 # 检查当前目录下是否存在 cJSON 目录
 if [ ! -d "cJSON" ]; then
     # 若不存在 cJSON 目录，则执行 git clone 操作
     git clone git@github.com:DaveGamble/cJSON.git
-    if [ $? -eq 0 ]; then
+    if [ $? -ne 0 ]; then
         echo "clone 'cJson' false or directory already existing."
     fi
 fi
 (cd cJSON && make all) # 在子shell中执行这些指令，这样就不会影响当前所在的目录
 
-if [ $? -eq 0 ]; then
-   echo "cJSO:make had do nothing!!!"
+if [ $? -ne 0 ]; then
+   echo "cJSON:make false!!!"
 fi
 
+####################### spdlog #########################
 if [ ! -d "spdlog" ]; then
     git clone git@github.com:gabime/spdlog.git
-    if [ $? -eq 0 ]; then
+    if [ $? -ne 0 ]; then 
         echo "clone 'spdlog' false or directory already existing."
     fi
 fi
 
-if [ $? -eq 0 ]; then
-    echo "spdlog:make had do nothing!!!"
+(cd spdlog && mkdir build && cd build && cmake .. && cmake --build .)
+if [ $? -ne 0 ]; then
+    echo "spdlog:complied false!!!"
 fi

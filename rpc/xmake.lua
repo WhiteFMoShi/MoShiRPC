c9a33b06d16b7bcdda900a2389bd9e56f8abebe8
xfy's WSL <3511784747@qq.com>
2026-02-12 13:14:22 +0800

    build(rpc): 更新构建方式为xmake构建

---
+++ rpc/xmake.lua
@@ -0,0 +1,22 @@
+---------- module global ----------
+set_languages("c++17")
+set_version("1.0.0")
+set_rules("mode.release", "mode.debug")
+set_rules("plugin.compile_commands.autoupdate")
+
+set_warnings("all", "error")
+add_requires("spdlog 1.16.*", 
+            "cjson 1.7.*", 
+            "protobuf-cpp 33.2",
+            "gtest 1.17.*")
+
+-- 在homebrew中找package(第三方仓库中的包)
+-- 需要使用vpn下载，不然没啥用
+-- add_requires("homebrew::zookeeper")
+
+target("rpc_core")
+    set_kind("static") -- 生成静态库
+
+    add_deps("network",
+            "protocol",
+            "register")

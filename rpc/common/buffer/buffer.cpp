1be7a52205a0a3c9b32609faf7b4300c6d50253d
WhiteFMoShi <3511784747@qq.com>
2026-02-16 23:53:26 +0800

    feat(rpc/common/buffer): 持续改进buffer的设计，完善buffer

--- rpc/common/buffer/buffer.cpp
+++ rpc/common/buffer/buffer.cpp
@@ -3,7 +3,7 @@
 
 ChainedBuffer::ChainedBuffer(uint node_size) : r_point_(nullptr), w_point_(nullptr),
                                 head_node_(nullptr), tail_node_(nullptr),
-                                node_count_(0), SINGLE_NODE_SIZE_OF_KB(node_size)
+                                size_(0), SINGLE_NODE_SIZE_OF_BYTE(node_size)
 {
 }
 
@@ -12,25 +12,59 @@ ChainedBuffer::~ChainedBuffer() {
 }
 
 int ChainedBuffer::write(const void* data, int len) {
-    // int offset = 0;
-    // while(BufferNode* temp_node = allocate_node_()) {
-    //     if(len > temp_node->NODE_SIZE) { // 一个结点放不下
-    //         set_node_data_(temp_node, static_cast<const char*>(data) + offset, temp_node->NODE_SIZE);
-    //         set_node_flag_(temp_node, offset == 0, false, true);
-    //         offset += temp_node->NODE_SIZE;
-    //     }
-    //     else { // 能放下
-    //         set_node_data_(temp_node, static_cast<const char*>(data) + offset, len - offset);
-    //         offset += len - offset;
-    //         set_node_flag_(temp_node, offset == 0, true, false);
-    //     }
-    // }
-    // return offset;
+    int offset = 0;
+    BufferNode* temp_node = nullptr;
+    BufferNode* mark = tail_node_;
+    while((temp_node = create_node_()) != nullptr) {
+        if(len > SINGLE_NODE_SIZE_OF_BYTE) { // 一个结点放不下
+            set_node_data_(temp_node, static_cast<const char*>(data) + offset, SINGLE_NODE_SIZE_OF_BYTE);
+            set_node_flag_(temp_node, offset == 0, false, true);
+            offset += SINGLE_NODE_SIZE_OF_BYTE;
+        }
+        else { // 能放下
+            set_node_data_(temp_node, static_cast<const char*>(data) + offset, len - offset);
+            offset += len - offset;
+            set_node_flag_(temp_node, offset == 0, true, false);
+        }
+        // 链接结点
+        insert_(tail_node_, temp_node);
+    }
+    // 存在分配失败的情况，需要回滚(不实际删除结点!)
+    if(temp_node == nullptr) {
+        while(temp_node != mark) {
+            temp_node->clear_node();
+        }
+    }
+    return offset;
 }
 
-void ChainedBuffer::set_node_data_(BufferNode* node, const void* data, int len) {
-    memcpy(node->data, data, len);
-    node->used_size = len;
+void ChainedBuffer::insert_(BufferNode* ds, BufferNode* rs) {
+    ds->next = rs;
+    rs->prev = ds;
+    if(rs->is_used()) {
+        size_++;
+    }
+    capacity_++;
+}
+
+BufferNode* ChainedBuffer::create_node_() {
+    BufferNode* node = new BufferNode(SINGLE_NODE_SIZE_OF_BYTE);
+    capacity_++;
+    return node;
+}
+
+void ChainedBuffer::del_node_(BufferNode* node) {
+    if(node == nullptr) {
+        return;
+    }
+    if(node->prev != nullptr) {
+        node->prev->next = node->next;
+    }
+    if(node->next != nullptr) {
+        node->next->prev = node->prev;
+    }
+    capacity_--;
+    delete node;
 }
 
 void ChainedBuffer::set_node_flag_(BufferNode* node, bool HEAD_FLAG, bool TAIL_FLAG, bool MORE_FLAG) {
@@ -39,3 +73,11 @@ void ChainedBuffer::set_node_flag_(BufferNode* node, bool HEAD_FLAG, bool TAIL_F
     node->MORE_FLAG = MORE_FLAG;
 }
 
+bool ChainedBuffer::set_node_data_(BufferNode* node, const void* data, int len) {
+    if(len <= sizeof(node->data)) {
+        memcpy(node->data, data, len);
+        node->used_size = len;
+        return true;
+    }
+    return false;
+}
--- rpc/common/buffer/buffer.hpp
+++ rpc/common/buffer/buffer.hpp
@@ -4,7 +4,50 @@
 #include <sys/types.h>
 #include <vector>
 
+/**
+ * @brief 缓冲区节点,进行数据的实际存储
+ * 
+ */
 struct BufferNode {
+    BufferNode(uint node_size) : next(nullptr), prev(nullptr), 
+                                MORE_FLAG(false), HEAD_FLAG(false), TAIL_FLAG(false), 
+                                used_size(0), read_offset(0), write_offset(0), 
+                                data(new char[node_size]) {}
+    ~BufferNode() {
+        if(data != nullptr) {
+            delete[] data;
+            data = nullptr;
+        }
+    }
+
+    /**
+     * @brief 删除缓冲区节点中的数据存储区，释放内存
+     * 
+     */
+    void delete_node_data() {
+        if(data != nullptr) {
+            delete[] data;
+            data = nullptr;
+        }
+    }
+
+    /**
+     * @brief 清空缓冲区节点的数据，不删除节点
+     * 
+     */
+    void clear_node() {
+        used_size = 0;
+        read_offset = 0;
+        write_offset = 0;
+
+        MORE_FLAG = false;
+        HEAD_FLAG = false;
+        TAIL_FLAG = false;
+    }
+
+    bool is_used() const { return used_size > 0; }
+
+public:
     BufferNode* next;
     BufferNode* prev;
 
@@ -21,7 +64,7 @@ struct BufferNode {
 };
 
 /**
- * @brief 链式缓冲区，用于存储和管理TCP报文
+ * @brief 链式缓冲区,仅仅存储数据,不进行其它的任何操作
  * 
  */
 class ChainedBuffer {
@@ -29,9 +72,10 @@ public:
     /**
     * @brief Construct a new Chained Buffer object
     * 
-    * @param node_size 单个
+    * @param node_size 单个缓冲区节点的大小，单位为BYTE
+    * @param max_node_count 最大缓冲区节点数量
     */
-    ChainedBuffer(uint node_size);
+    ChainedBuffer(uint node_size, uint max_node_count);
     ~ChainedBuffer();
 
     ChainedBuffer(ChainedBuffer&&) = delete;
@@ -43,7 +87,7 @@ public:
      * 
      * @param data 
      * @param len 
-     * @return int 写入的字节数
+     * @return int 写入的字节数, 写入失败返回-1
      */
     int write(const void* data, const int len);
 
@@ -51,20 +95,14 @@ public:
      * @brief 尝试直接获取一个完整的报文，将数据存入ds中
      * 
      * @param ds 
-     * @return int 读取的字节数
+     * @return int 读取的字节数, 读取失败返回-1
      */
     int read(void* ds, const int len);
 
-    /**
-     * @brief 尝试从p开始，解析一个完整报文
-     * 
-     * @param p 
-     * @return bool true：p开始的报文完整； false：p开始的报文不完整
-     */
-    bool try_to_parse_from(const BufferNode* p) const;
-
-    bool empty() const;
+    bool empty() const { return size_ == 0; }
     void clear();
+
+    uint get_node_count() const { return size_; }
 
 private:
     /**
@@ -83,10 +121,10 @@ private:
     void remove_(BufferNode* ds);
 
     /* node操作 */
-    BufferNode* allocate_node_(); // 单纯的创建node
-    void del_node_(BufferNode*); // 单纯的清理node
+    BufferNode* create_node_(); // 创建node
+    void del_node_(BufferNode*); // 删除node
     void set_node_flag_(BufferNode* node, bool HEAD_FLAG, bool TAIL_FLAG, bool MORE_FLAG);
-    void set_node_data_(BufferNode* node, const void* data, int len);
+    bool set_node_data_(BufferNode* node, const void* data, int len);
 
 private:
     BufferNode* r_point_;
@@ -95,6 +133,8 @@ private:
     BufferNode* head_node_;
     BufferNode* tail_node_;
 
-    uint node_count_;
-    uint SINGLE_NODE_SIZE_OF_KB;
+    uint size_;
+    uint capacity_;
+    uint SINGLE_NODE_SIZE_OF_BYTE;
+    uint MAX_NODE_COUNT;
 };

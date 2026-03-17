e892c17a1c9cde2e0c14da351fccd12fcecc83d1
xfy's WSL <3511784747@qq.com>
2026-03-18 00:05:02 +0800

    fix(rpc/network.channel): 重写Channel，修复其中的大量问题，但是问题没有全部解决完
    遗留的问题：
    	1. 对于错误处理的逻辑不正确，不符合实际的要求
    	2. 没有处理EPOLLHUP/EPOLLRDHUP两种情况，因为我对这两种还不了解，等学习了之后再来进行修改

--- rpc/network/include/channel.hpp
+++ rpc/network/include/channel.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cstdint>
 #include <functional>
 #include <sys/epoll.h>
 #include <sys/types.h>
@@ -8,6 +9,10 @@
 
 namespace moshi{
 
+/**
+* @brief 专注于进行事件的回调处理
+* 
+*/
 class Channel {
 public:
     using EpollinCallback  = std::function<void()>;
@@ -15,33 +20,45 @@ public:
     using EpollerrCallback = std::function<void()>;
 
 public:
+    Channel() : fd_(-1), focus_events_(0) {}
+
     void set_fd(int fd) { fd_ = fd; }
     int  get_fd() const { return fd_; }
 
-    void set_epoll_events(epoll_event events) { epoll_events_ = events; }
-    epoll_event* get_epoll_events() { return &epoll_events_; }
+    void set_focus_events(uint32_t fc_events) { focus_events_ = fc_events; }
+    uint32_t get_focus_events() const { return focus_events_; }
 
     void set_epollin_callback (EpollinCallback cb);
     void set_epollout_callback(EpolloutCallback cb);
     void set_epollerr_callback(EpollerrCallback cb);
 
-    void remove_epollin_callback()  { epoll_events_.events &= ~EPOLLIN; }
-    void remove_epollout_callback() { epoll_events_.events &= ~EPOLLOUT; }
-    void remove_epollerr_callback() { epoll_events_.events &= ~EPOLLERR; }
+    void remove_epollin_event()  { focus_events_ &= ~EPOLLIN; }
+    void remove_epollout_event() { focus_events_ &= ~EPOLLOUT; }
+    void remove_epollerr_event() { focus_events_ &= ~EPOLLERR; }
 
     EpollinCallback  get_readable_callback() const { return epollin_cb_; }
     EpolloutCallback get_writable_callback() const { return epollout_cb_; }
-    EpollerrCallback get_error_callback()     const { return epollerr_cb_; }
+    EpollerrCallback get_error_callback()    const { return epollerr_cb_; }
     
-    void handle_event() const;
-    void handle_error() const;
+    /**
+     * @brief 根据epoll_wait触发的事件来进行函数回调操作
+     * 
+     * @param revent epoll_wait所返回的，已被触发的事件
+     */
+    void handle_event(uint32_t revent) const;
+
 private:
+    void handle_error_() const;
+
+private:
+    // 事件对应的回调函数
     EpollinCallback  epollin_cb_;
     EpolloutCallback epollout_cb_;
     EpollerrCallback epollerr_cb_;
 
-    epoll_event epoll_events_;
+    // epoll_event focus_events_; // 关注的事件handle
     int fd_;
+    uint32_t focus_events_;
 };
 
 } // namespace moshi
--- rpc/network/src/channel.cpp
+++ rpc/network/src/channel.cpp
@@ -1,25 +1,26 @@
 #include "channel.hpp"
-#include "event_loop.hpp"
+#include <utility>
+#include <sys/epoll.h>
 
 using moshi::Channel;
 
-void Channel::handle_event() const {
-    if(epoll_events_.events & EPOLLERR) { // 错误拥有最高的优先级
-        handle_error();
+void Channel::handle_event(uint32_t revent) const {
+    if(revent & focus_events_ & EPOLLERR) { // 错误拥有最高的优先级
+        handle_error_();
         return;
     }
-    if(epoll_events_.events & EPOLLIN) {
+    if(revent & focus_events_ & EPOLLIN) {
         if(epollin_cb_ != nullptr)
             epollin_cb_();
     }
-    if(epoll_events_.events & EPOLLOUT) {
+    if(revent & focus_events_ & EPOLLOUT) {
         if(epollout_cb_ != nullptr)
             epollout_cb_();
     }
 }
 
-void Channel::handle_error() const {
-    if(epoll_events_.events & EPOLLERR) { // 设置了错误回调才可执行
+void Channel::handle_error_() const {
+    if(focus_events_ & EPOLLERR) { // 设置了错误回调才可执行
         if(epollerr_cb_ != nullptr)
             epollerr_cb_();
     }
@@ -27,15 +28,15 @@ void Channel::handle_error() const {
 
 void Channel::set_epollout_callback(EpolloutCallback cb) {
     epollout_cb_ = std::move(cb);
-    epoll_events_.events |= EPOLLOUT;
+    focus_events_ |= EPOLLOUT;
 }
 
 void Channel::set_epollin_callback(EpollinCallback cb) {
     epollin_cb_ = std::move(cb);
-    epoll_events_.events |= EPOLLIN;
+    focus_events_ |= EPOLLIN;
 }
 
 void Channel::set_epollerr_callback(EpollerrCallback cb) {
     epollerr_cb_ = std::move(cb);
-    epoll_events_.events |= EPOLLERR;
+    focus_events_ |= EPOLLERR;
 }
